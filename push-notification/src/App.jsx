04780b75d29ea659319d0b69de7d777aa6f3505c
ajay <ajay@sgate.net>
2023-12-04 16:03:46 +0530

    submit

---
+++ push-notification/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import addNotification from 'react-push-notification';
+import logo from './images/logo.jpg';
+
+function App() {
+
+  const handleNotify = () => {
+    console.log("clicked");
+
+    addNotification({
+      title: "sGate Tech Solutions Pvt. Ltd.",
+      message: "Notification Message",
+      duration: 10000,
+      icon: logo,
+      native: true,
+      onClick: ()=>window.location = "https://www.sgate.in/"
+    });
+  }
+
+  return (
+    <>
+      <button style={{ margin: "200px" }} onClick={handleNotify}>Click to notify</button>
+    </>
+  )
+}
+
+export default App
